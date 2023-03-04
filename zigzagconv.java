6052dfaa2adab052dce77756c88baf2ff0715dc4
ninja011003 <niranjan011003@gmail.com>
2023-03-04 23:02:51 +0530

    using queue

--- zigzagconv.java
+++ zigzagconv.java
@@ -1,3 +1,6 @@
+import java.util.LinkedList;
+import java.util.Queue;
+
 public class zigzagconv {
     /*
      * The string "PAYPALISHIRING" is written in a zigzag pattern on a given number of rows like this: (you may want to display this pattern in a fixed font for better legibility)
@@ -12,12 +15,33 @@ public class zigzagconv {
      * Output: "PAHNAPLSIIGYIR"
      */
     public static String convert(String s, int numRows) {
-        StringBuffer str = new StringBuffer(s);
         StringBuffer ans = new StringBuffer();
-        while(str.length()!=0){
-            
+        Queue<Character>[] bundle= new Queue[numRows];
+        for(int i=0;i<numRows;i++){
+            bundle[i]= new LinkedList<Character>();
+        }
+        int flag =1,k=0;
+        for(int i=0;i<s.length();i++){
+            bundle[k].add(s.charAt(i));
+            if(numRows==1)
+                continue;
+            if(k==numRows-1)
+                flag=-1;
+            if(k==0)
+                flag=1;
+            if(flag>0)
+                k++;
+            if(flag<0)
+                k--;
+        }
+        for(int i=0;i<numRows;i++){
+            while(!bundle[i].isEmpty()){
+                ans.append(bundle[i].poll());
+            }
         }
         return ans.toString();
-        
+    }
+    public static void main(String[] args) {
+        System.out.println(convert("PAYPALISHIRING", 3));
     }
 }
